bad6815592e52c6ead33b2fb5473617378101665
Kunakorn <kunakorn.ph98@gmail.com>
2024-06-06 14:54:27 +0700

    Assignmeny#1 React Manipulating Array March

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,25 +1,58 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [messageData, setMessage] = useState([
+    "Hello all ! This is first message.",
+  ]);
+  const [addData, setAddData] = useState("");
+  // Not work
+  /*   const handleSubmit = (e) => {
+    e.preventDefault()
+    setAddData(e.target.messageText.value)
+    const ghostArr = [...messageData]
+    ghostArr.push(addData)
+    setMessage(ghostArr)
+  } */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const ghostArr = [...messageData];
+    ghostArr.push(addData);
+    setMessage(ghostArr);
+  };
+  const hadleDelete = (index) => {
+    const ghostArr = [...messageData];
+    ghostArr.splice(index,1);
+    setMessage(ghostArr)
+  }
   return (
-    <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
-        <label>
-          <input
-            id="message-text"
-            name="message-text"
-            type="text"
-            placeholder="Enter message here"
-          />
-        </label>
-        <button className="submit-message-button">Submit</button>
-      </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
+      <div className="app-wrapper">
+        <h1 class="app-title">Message board</h1>
+        <div class="message-input-container">
+          <label>
+            <input
+              id="message-text"
+              name="messageText"
+              type="text"
+              placeholder="Enter message here"
+              value={addData}
+              onChange={(event) => setAddData(event.target.value)}
+            />
+          </label>
+          <button className="submit-message-button" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+        <div class="board">
+          {messageData.map((msg, index) => {
+            return (
+              <div className="message">
+                <h1 key={index}>{msg}</h1>
+                <button className="delete-button" onClick={() => hadleDelete(index)}>x</button>
+              </div>
+            );
+          })}
         </div>
       </div>
-    </div>
   );
 }
 
